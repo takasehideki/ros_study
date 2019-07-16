71bdba87f091054c3148bd68c522348cc959d004
takasehideki <takase@i.kyoto-u.ac.jp>
2019-07-16 15:22:15 +0900

    devel,buildを削除

--- ros1_melodic/catkin_ws/build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/takase/ros_study/ros1_melodic/catkin_ws/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
