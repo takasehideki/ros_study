1b3e31623471823c93e1710ea7e05383f40f2446
takasehideki <takase@i.kyoto-u.ac.jp>
2019-07-17 15:31:20 +0900

    topicノードのコンポーネント実装

--- ros2_dashing/ros2_ws/src/pubsub_component/src/talker.cpp
+++ ros2_dashing/ros2_ws/src/pubsub_component/src/talker.cpp
@@ -28,13 +28,8 @@
 // %Tag(ROS_HEADER)%
 #include "rclcpp/rclcpp.hpp"
 // %EndTag(ROS_HEADER)%
-// %Tag(MSG_HEADER)%
-#include "std_msgs/msg/string.hpp"
-// %EndTag(MSG_HEADER)%
 
-#include <iostream>
-#include <chrono>
-using namespace std::chrono_literals;
+#include "pubsub_component/talker_component.hpp"
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
@@ -61,77 +56,12 @@ int main(int argc, char **argv)
    * NodeHandle destructed will close down the node.
    */
 // %Tag(NODEHANDLE)%
-  auto n = rclcpp::Node::make_shared("talker");
+  auto n = std::make_shared<pubsub_component::Talker>();
 // %EndTag(NODEHANDLE)%
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-// %Tag(PUBLISHER)%
-  auto chatter_pub = n->create_publisher<std_msgs::msg::String>("chatter", 1000);
-// %EndTag(PUBLISHER)%
-
-// %Tag(LOOP_RATE)%
-  rclcpp::WallRate loop_rate(100ms);
-// %EndTag(LOOP_RATE)%
-
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-// %Tag(ROS_OK)%
-  auto count = 0;
-  while (rclcpp::ok())
-  {
-// %EndTag(ROS_OK)%
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-// %Tag(FILL_MESSAGE)%
-    std_msgs::msg::String msg;
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-// %EndTag(FILL_MESSAGE)%
-
-// %Tag(ROSCONSOLE)%
-    RCLCPP_INFO(n->get_logger(), "%s", msg.data.c_str());
-// %EndTag(ROSCONSOLE)%
-
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-// %Tag(PUBLISH)%
-    chatter_pub->publish(msg);
-// %EndTag(PUBLISH)%
-
-// %Tag(SPINONCE)%
-    rclcpp::spin_some(n);
-// %EndTag(SPINONCE)%
-
-// %Tag(RATE_SLEEP)%
-    loop_rate.sleep();
-// %EndTag(RATE_SLEEP)%
-    ++count;
-  }
+// %Tag(SPIN)%
+  rclcpp::spin(n);
+// %EndTag(SPIN)%
 
   rclcpp::shutdown();
   return 0;
