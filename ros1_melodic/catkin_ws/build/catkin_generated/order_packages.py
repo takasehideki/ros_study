f627afdc521b94603f886f0d0eb15b9b00357f67
takase <takase@i.kyoto-u.ac.jp>
2020-02-11 13:32:28 +0900

    ワークスペースの追加と設定

---
+++ ros1_melodic/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/takase/ros_study/ros1_melodic/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/takase/ros_study/ros1_melodic/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/takase/ros_study/ros1_melodic/catkin_ws/devel;/opt/ros/melodic" != "" else []
---
+++ ros1_melodic/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH="/opt/ros/melodic/lib"
+export PWD="/home/takase/ros_study/ros1_melodic/catkin_ws/build"
