962c6edda002ec084169986ecea4172f686b565b
takase <takase@i.kyoto-u.ac.jp>
2020-02-11 13:29:43 +0900

    いったん削除

--- ros1_melodic/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/takase/ros_study/ros1_melodic/catkin_ws/src
-# Build directory: /home/takase/ros_study/ros1_melodic/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- ros1_melodic/catkin_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/takase/ros_study/ros1_melodic/catkin_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
