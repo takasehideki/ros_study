cd34d69f4afdd085e83144b1936642a9e27d47ce
takasehideki <takase@i.kyoto-u.ac.jp>
2020-02-15 18:51:05 +0900

    pubsub_compositionのlistener実装

--- ros2_dashing/ros2_ws/src/pubsub_composition/src/listener.cpp
+++ ros2_dashing/ros2_ws/src/pubsub_composition/src/listener.cpp
@@ -27,19 +27,7 @@
 
 // %Tag(FULLTEXT)%
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-
-rclcpp::Node::SharedPtr n = nullptr;
-
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-// %Tag(CALLBACK)%
-void chatterCallback(const std_msgs::msg::String::SharedPtr msg)
-{
-  RCLCPP_INFO(n->get_logger(), "I heard: [%s]", msg->data.c_str());
-}
-// %EndTag(CALLBACK)%
+#include "pubsub_component/listener_component.hpp"
 
 int main(int argc, char **argv)
 {
@@ -55,44 +43,16 @@ int main(int argc, char **argv)
    */
   rclcpp::init(argc, argv);
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  n = rclcpp::Node::make_shared("listener");
+  rclcpp::executors::SingleThreadedExecutor exec;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-// %Tag(SUBSCRIBER)%
-  auto sub = n->create_subscription<std_msgs::msg::String>("chatter", 1000, chatterCallback);
-// %EndTag(SUBSCRIBER)%
+  auto listener = std::make_shared<pubsub_component::Listener>();
+  exec.add_node(listener);
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
 // %Tag(SPIN)%
-  rclcpp::spin(n);
+  exec.spin();
 // %EndTag(SPIN)%
 
   rclcpp::shutdown();
-  sub = nullptr;
-  n = nullptr;
   return 0;
 }
 // %EndTag(FULLTEXT)%
